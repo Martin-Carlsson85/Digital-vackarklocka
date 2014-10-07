c9ac01a40bd22608c2fbb99a6749af09d8095c93
mc22dg <mc22dg@student.lnu.se>
2014-10-07 15:40:13 +0200

    Deklarerat fält, metoder, konstruktorer m.m. som behövs för up.

---
+++ Digital_vackarklocka/AlarmClock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_vackarklocka
+{
+    class AlarmClock
+    {   
+        //Fält
+        private int _alarmaHour;
+        private int _alarmaMinute;
+        private int _hour;
+        private int _minute;
+
+
+        //Egenskaper
+        public int AlarmMinute
+        {
+            get { return}
+
+            set {}
+        }
+
+        public int AlarmMinute
+        {
+
+        }
+
+        public int Hour
+        { 
+        
+        }
+
+        public int Minute
+        { 
+        
+        }
+
+        //Kunstruktorer med parameterlista
+        public AlarmClock()
+        {
+
+        }
+
+
+        public AlarmClock(int hour, int minute)
+        {
+
+        }
+
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+
+        }
+
+
+
+        //Metoder
+        public void TickTock();
+
+
+        public void ToString();
+    }
+}
---
+++ Digital_vackarklocka/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_vackarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+               
+
+       }
+
+
+        static void Run()
+        { 
+        
+        }
+
+        static void ViewErrorMessage()
+        {
+
+        }
+
+        static void ViewTestHeader()
+        {
+
+        }
+    }
+}
